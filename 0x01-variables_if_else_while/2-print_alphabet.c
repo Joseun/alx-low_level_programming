c3aae27bb461d7e8c045ae09513cba348324efdf
Joseun <ologanj@gmail.com>
2021-08-27 17:10:33 +0000

    Variables and Correction

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char ch = a;
+
+	for (char = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
