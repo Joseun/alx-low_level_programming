a04051a4ab564223d5f502f8dbebf95457585a78
Joseun <ologanj@gmail.com>
2021-08-31 20:52:06 +0000

    functions with library declaration

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - computes and prints the absolute value of a number
+ *
+ * @n: integer input number
+ *
+ * Return: absolute value
+ */
+
+int _abs(int n)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	else
+	{
+		return (i * -1);
+	}
+	_putchar('\n');
+}
