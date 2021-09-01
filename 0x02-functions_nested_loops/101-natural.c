5dc86032ec70a86bb734ee2726a136faec25c51f
Joseun <ologanj@gmail.com>
2021-09-01 20:58:17 +0000

    Advanced nested loops

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,17 +6,18 @@
  * Return: 0
  */
 
-int main(void)
+int main()
 {
-	int x, y = 0;
+	int x, y;
 
 	for (x = 0; x < 1024; x++)
 	{
 		if ((x % 3) == 0 || (x % 5) == 0)
 		{
+			y = 0;
 			y += x;
 		}
 			printf("%d\n", y);
-			return (0);
+			return (y);
 	}
 }
