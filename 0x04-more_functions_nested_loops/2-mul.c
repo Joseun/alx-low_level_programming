7010c8665da4c24afbce76fb7f07eea0fcded320
Joseun <ologanj@gmail.com>
2021-09-02 12:41:41 +0000

    More nested fuctions and debugging

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers and returns the result
+ *
+ * @n: integer input number
+ *
+ * @m: integer input number
+ *
+ * Return: the result of multiplication of n and m
+ */
+
+int mul(int n, int m)
+{
+	int a;
+
+	a = n * m;
+
+	return (a);
+}
