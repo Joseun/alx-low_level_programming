4e13a17891acba9449805680053d571006722db8
Joseun <ologanj@gmail.com>
2021-09-03 20:14:37 +0000

    More nested fuctions and debugging

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,13 +18,13 @@ int main(void)
 		{
 			printf("FizzBuzz ");
 		}
+		else if (x % 5 == 0)
+		{
+			printf("Buzzz ");
+		}
 		else if (x % 3 == 0)
 		{
 			printf("Fizz ");
-		}
-		else if (x % 5 == 0)
-		{
-			printf("Buzz ");
 		}
 		else
 		{
