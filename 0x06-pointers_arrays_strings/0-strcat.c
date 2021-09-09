cdcfe4ce2f82049576442538e4e9d44d31fd711e
Joseun <ologanj@gmail.com>
2021-09-09 20:00:58 +0000

    Work is hard

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,39 +1,31 @@
 #include "main.h"
 
 /**
- * *_strcat - appends a string to another
+ * _strcat - appends a string to another
  *
- * @dest: pointer to buffer
- * @src: pointer to string
+ * @dest: string ton append to
+ * @src: string to add
  *
- * Return: the pointer to @dest
+ * Return: the pointer to resulting string
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int len = 0, y = 0, x = 0, z = 0, a, destlen, srclen;
+	int y = 0, x = 0;
 
 	while (dest[x] != '\0')
 	{
 		x++;
-		len++;
 	}
-
-	destlen = len;
 
 	while (src[y] != '\0')
 	{
+		dest[x] =  src[y];
 		y++;
-		z++;
+		x++
 	}
 
-	srclen = z;
-
-	for (a = 0; a < srclen; a++)
-	{
-		dest[destlen + a] = src[a];
-	}
-	dest[destlen + a] = '\0';
+	dest[x] = '\0';
 
 	return (dest);
 }
