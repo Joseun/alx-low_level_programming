c25b8dcab3d1d2d109ec1ee61601ad5a078e7fe8
Joseun <ologanj@gmail.com>
2021-09-17 20:39:34 +0000

    Correction to add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	{
 		for (y = 0; argv[x][y] != 0; y++)
 		{
-			if (argv[x][y] < 48 && argv[x][y] > 57)
+			if (argv[x][y] < 48 || argv[x][y] > 57)
 			{
 				printf("Error\n");
 				return (1);
