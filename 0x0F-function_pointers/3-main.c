5e5136d68a0a109041498432ffbd8cb314fd9465
Joseun <ologanj@gmail.com>
2021-10-01 17:37:15 +0000

    Function pointers exercises

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,8 +13,7 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	char op;
-	int (*func)(int, int);
+	char *op;
 
 	if (argc != 4)
 	{
@@ -24,6 +23,7 @@ int main(int argc, char *argv[])
 
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
+	op = argv[2];
 
 	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
@@ -31,15 +31,13 @@ int main(int argc, char *argv[])
 		exit(99);
 	}
 
-	op = argv[2];
-
-	if ((op == '/' || op == '%') && num2 == 0)
+	if ((*op == '/' || *op == '%') && num2 == 0)
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = get_op_func(num1, num2);
+	result = get_op_func(op)(num1, num2);
 
 	printf("%d\n", result);
 
