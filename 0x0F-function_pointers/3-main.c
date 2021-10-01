bca62f317683c4c23f6d8fd8a7d1180922aaaa47
Joseun <ologanj@gmail.com>
2021-10-01 16:09:27 +0000

    Function pointers exercises

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,47 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stlib.h>
+
+/**
+ * main - program that perfroms simple operations
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int num1, num2, result;
+	char op;
+	int (*func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	op = argv[2];
+
+	if ((op == '/' || op == '%') && num2 == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = get_op_func(num1, num2);
+
+	printf("%d\n", result);
+
+	return (0);
+}
