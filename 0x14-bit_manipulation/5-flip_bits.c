dd9b422df77eb5650ab138c701191d1ba492b662
Joseun <ologanj@gmail.com>
2021-10-18 09:50:57 +0000

    Bit manipulation exercises

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * flip_bits - Counts the number of bits needed to be
+ *             flipped to get from one number to another.
+ * @n: The number.
+ * @m: The number to flip n to.
+ *
+ * Return: The necessary number of bits to flip to get from n to m.
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned int difference = m ^ n;
+	while (difference)
+		difference &= (difference - 1);
+	return difference;
+}
