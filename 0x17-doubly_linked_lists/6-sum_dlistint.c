79d9d8815576844f1ef90321405b6430f27e96ab
Joseun <ologanj@gmail.com>
2021-11-25 18:55:17 +0000

    Doubly linked lists

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,10 +1,28 @@
 #include "lists.h"
 
 /**
- * dlistint_len - Prints the number of elements in a double linked
+ * sum_dlistint - sums all the elements in a double linked
  * list
  *
- * @h: the double linked list
+ * @head: the double linked list first element
  *
- * Return: the number of nodes iin h if successful
+ * Return: the number of sum of the elements in the list
+ *       or 0. if NULL
  */
+
+int sum_dlistint(dlistint_t *head)
+{
+	long int sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	while (head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
