c7818988fa2445590ea648e7cba2b544cbab9227
Joseun <ologanj@gmail.com>
2021-11-25 18:28:41 +0000

    README.md attached

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - Prints the number of elements in a double linked
+ * list
+ *
+ * @h: the double linked list
+ *
+ * Return: the number of nodes iin h if successful
+ */
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,5 @@
+0x17. C - Doubly linked lists
+
+0. Print list
+mandatory
+Write a function that prints all the elements of a dlistint_t list
