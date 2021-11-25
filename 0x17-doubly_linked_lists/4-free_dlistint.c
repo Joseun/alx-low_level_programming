ba9429a84ede47e3f1cb7654bb60ea0a23e11eb8
Joseun <ologanj@gmail.com>
2021-11-25 18:24:33 +0000

    Doubly linked lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - Frees a listint_t list.
+ * @head: A pointer to the head of the listint_t list to be freed.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp_1;
+
+	tmp_1 = head->next;
+
+	while (tmp_1)
+	{
+		dlistint_t *tmp_2 = tmp_1;
+
+		tmp_1 = tmp_2->next;
+
+		free(tmp_2);
+	}
+	free(head);
+}
---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Locates a given node of
+ *                        a dlistint_t linked list.
+ * @head: A pointer to the head of the listint_t list.
+ * @index: The index of the node to locate - indices start at 0.
+ *
+ * Return: If the node does not exist - NULL.
+ *         Otherwise - the located node.
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	for (count = 0; count < index; count++)
+	{
+		head = head->next;
+	}
+	return(head);
+}
