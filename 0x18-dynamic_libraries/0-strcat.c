579d3e9dfa4ba99ea3d30f51a8b64b393bd74981
Joseun <ologanj@gmail.com>
2021-11-29 12:09:51 +0000

    Dynamic libraries

---
+++ 0x18-dynamic_libraries/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strcat - appends a string to another
+ *
+ * @dest: string ton append to
+ * @src: string to add
+ *
+ * Return: the pointer to resulting string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int y = 0, x = 0;
+
+	while (dest[x] != '\0')
+	{
+		x++;
+	}
+	while (src[y] != '\0')
+	{
+		dest[x] =  src[y];
+		y++;
+		x++;
+	}
+	dest[x] = '\0';
+	return (dest);
+}
