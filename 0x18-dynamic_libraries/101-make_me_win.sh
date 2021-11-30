1319c961a9a25b02c1836c8986ad9f2cad96f4bc
Joseun <ologanj@gmail.com>
2021-11-30 02:01:27 +0000

    Dynamic Libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 gcc -shared -fPIC preload.c -o preload.so -ldl
-export LD_PRELOAD=$PWD/preload.so ldd gm
+export LD_PRELOAD=/home/vagrant/alx-low_level_programming/0x18-dynamic_libraries/preload.so ldd gm
