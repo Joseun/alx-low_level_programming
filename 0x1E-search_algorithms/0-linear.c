e1f09a0aea249429a1eb4aa675dd4f11e5623f81
Joseun <ologanj@gmail.com>
2022-06-15 05:48:38 +0000

    Left over work from search algorithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -7,10 +7,8 @@
  * @array: pointer to the first element of the array to search in
  * @size: number of elements in array
  * @value: value to search for
- *
  * Return: -1 if value is not present in array or if array is NULL, or value
  */
-
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
